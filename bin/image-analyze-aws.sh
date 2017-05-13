5dda9c4c458cdd295d3cde92fd81ac01048d3cbf
Lukasz Karolewski <lkarolewski@gmail.com>
2017-05-12 21:43:19 -0700

    updating params

--- bin/image-analyze-aws.sh
+++ bin/image-analyze-aws.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-export AWS_ACCESS_KEY_ID=$1
-export AWS_SECRET_ACCESS_KEY=$2
+inputFileName=$1
 
-inputFileName=$3
-
-AWS_PATH=$(dirname $3)
+AWS_PATH=$(dirname $inputFileName)
 FILE_NAME=$(echo "${inputFileName##*/}")
 FILE_NAME=$(echo "${FILE_NAME%.*}")
 
-echo "Analyzing file: $3"
+echo "Analyzing file: $inputFileName"
 echo "AWS path: $AWS_PATH"
 echo "File name: $FILE_NAME"
 
-aws s3 cp $3 /$FILE_NAME.GPR
+aws s3 cp $inputFileName /$FILE_NAME.GPR
 aws s3 cp $AWS_PATH/$FILE_NAME.json /$FILE_NAME.json
 
 # This is necessary to run Matlab Runtime Environment
