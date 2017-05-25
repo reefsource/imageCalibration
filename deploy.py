6c4e6d3b1fd43d60e28e762ac725c8bebab60926
Lukasz Karolewski <lkarolewski@gmail.com>
2017-05-24 23:40:26 -0700

    increasing memory reservation

--- deploy.py
+++ deploy.py
@@ -35,7 +35,7 @@ class DeploymentManager():
             "containerDefinitions": [{
                 "name": task_family,
                 "image": "078097297037.dkr.ecr.us-east-1.amazonaws.com/{task_family}:{image_tag}".format(task_family=task_family, image_tag=image_tag),
-                "memoryReservation": 384,
+                "memoryReservation": 768,
                 "environment": env_vars
             }]
         }
