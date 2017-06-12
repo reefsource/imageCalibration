8561eafde699b80d128635640ed2a36386f38b49
Lukasz Karolewski <lkarolewski@gmail.com>
2017-06-11 23:01:10 -0700

    added function to report errors

--- bin/image-analyze-aws.sh
+++ bin/image-analyze-aws.sh
@@ -12,6 +12,17 @@ AWS_PATH=$(dirname $inputFileName)
 FILE_NAME=$(echo "${inputFileName##*/}")
 FILE_NAME=$(echo "${FILE_NAME%.*}")
 
+function submitResult {
+    jq -n --arg upload_id "$upload_id" --arg stage "stage_2" --arg result "$1" '{uploaded_file_id: $upload_id, stage: $stage, json: $result}' | curl -v \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Token ${AUTH_TOKEN}" \
+        -X POST -d@- http://coralreefsource.org/api/v1/results/submit/
+}
+
+function submitError {
+   submitResult "{\"error\": \"$1\"}"
+}
+
 echo "Analyzing file: $inputFileName"
 echo "AWS path: $AWS_PATH"
 echo "File name: $FILE_NAME"
@@ -19,18 +30,10 @@ echo "File name: $FILE_NAME"
 aws s3 cp $inputFileName /$FILE_NAME.GPR
 aws s3 cp $AWS_PATH/${FILE_NAME}_stage1.json /$FILE_NAME.json
 
-# This is necessary to run Matlab Runtime Environment
-# But causes some conflicts with awscli
-# This is why we delete the LD_LIBRARY_PATH after the call
-export LD_LIBRARY_PATH="/opt/mcr/v901/runtime/glnxa64:/opt/mcr/v901/bin/glnxa64:/opt/mcr/v901/sys/os/glnxa64"
-/analyzeImage /$FILE_NAME.GPR "path" "/data"
-export LD_LIBRARY_PATH=""
-
+LD_LIBRARY_PATH="/opt/mcr/v901/runtime/glnxa64:/opt/mcr/v901/bin/glnxa64:/opt/mcr/v901/sys/os/glnxa64" /analyzeImage /$FILE_NAME.GPR "path" "/data"
 
 hasLatitude=$(jq '.GPSLatitude' /$FILE_NAME.json)
 hasLongitude=$(jq '.GPSLongitude' /$FILE_NAME.json)
-
-
 
 #If the .json file does not have geo-data, replace it
 if [[ -z $hasLatitude || -n $hasLatitude || -z $hasLongitude || -n $hasLongitude ]]
@@ -50,7 +53,4 @@ fi
 aws s3 cp $FILE_NAME"_labels.png" $AWS_PATH/$FILE_NAME"_labels.png" --acl 'public-read'
 aws s3 cp $FILE_NAME.json $AWS_PATH/${FILE_NAME}_stage2.json
 
-jq -n --arg upload_id "$upload_id" --arg stage "stage_2" '{uploaded_file_id: $upload_id, stage: $stage}' | curl -v \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Token ${AUTH_TOKEN}" \
-    -X POST -d@- http://coralreefsource.org/api/v1/results/stage2complete/
+submitResult "`cat ${FILE_NAME}_stage2.json`"
